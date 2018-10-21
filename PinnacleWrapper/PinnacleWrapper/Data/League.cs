9da5c6ee54e6cfe219d26a28150421b2df259ad1
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-10-21 16:50:55 +0300

    edit Ligue.cs

--- PinnacleWrapper/PinnacleWrapper/Data/League.cs
+++ PinnacleWrapper/PinnacleWrapper/Data/League.cs
@@ -5,18 +5,30 @@ namespace PinnacleWrapper.Data
     public class League
     {
         [JsonProperty("id")]
-        public int Id;
+        public int Id { get; set; }
 
         [JsonProperty("name")]
-        public string Name;
+        public string Name { get; set; }
 
         [JsonProperty("homeTeamType")]
-        public string HomeTeamType;
+        public string HomeTeamType { get; set; }
 
         [JsonProperty("hasOfferings")]
-        public bool HasOfferings;
+        public bool HasOfferings { get; set; }
+
+        [JsonProperty("container")]
+        public string Container { get; set; }
 
         [JsonProperty("allowRoundRobins")]
-        public string AllowRoundRobins;
+        public string AllowRoundRobins { get; set; }
+
+        [JsonProperty("leagueSpecialsCount")]
+        public int LeagueSpecialsCount { get; set; }
+
+        [JsonProperty("eventSpecialsCount")]
+        public int EventSpecialsCount { get; set; }
+
+        [JsonProperty("eventCount")]
+        public int EventCount { get; set; }
     }
 }
