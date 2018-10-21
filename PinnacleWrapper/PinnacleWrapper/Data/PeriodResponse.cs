9ca10e8ac10469eed4ff6671afd0eade9cfb75e8
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-10-21 17:43:35 +0300

    create PeriodResponse.cs

---
+++ PinnacleWrapper/PinnacleWrapper/Data/PeriodResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace PinnacleWrapper.Data
+{
+    public class PeriodResponse
+    {
+        [JsonProperty("periods")]
+        public List<Period> Periods { get; set; }
+    }
+}
