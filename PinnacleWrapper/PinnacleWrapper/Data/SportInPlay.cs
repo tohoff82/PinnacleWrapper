da0560dfb5a3e1614b0255861e49b9708a38a892
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-10-22 05:58:35 +0300

    create SportInPlay object

---
+++ PinnacleWrapper/PinnacleWrapper/Data/SportInPlay.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PinnacleWrapper.Data
+{
+    public class SportInPlay
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("leagues")]
+        public List<LigueInPlay> LiguesList { get; set; }
+    }
+}
