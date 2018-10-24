cf513b3b5f90ffac54fdc159836ab2869895c8b6
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-10-24 18:34:59 +0300

    create TeaserGroupsResponse.cs

---
+++ PinnacleWrapper/PinnacleWrapper/Data/Response/TeaserGroupsResponse.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace PinnacleWrapper.Data.Response
+{
+    public class TeaserGroupsRespose
+    {
+        [JsonProperty("teaserGroups")]
+        public List<TeaserGroup> TeaserGroups { get; set; }
+    }
+}
