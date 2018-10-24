57ee8e88eda5d88a83308ca4adcd92850c58c37d
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-10-24 18:16:44 +0300

    create CancellationReasons.cs

---
+++ PinnacleWrapper/PinnacleWrapper/Data/CancellationReason.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace PinnacleWrapper.Data
+{
+    public class CancellationReason
+    {
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+    }
+}
