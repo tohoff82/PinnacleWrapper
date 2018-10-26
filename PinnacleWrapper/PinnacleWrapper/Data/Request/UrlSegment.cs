de6d4d19f1b88957d1fe0743661a7f90570cce62
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-10-26 16:53:54 +0300

    add straightLine segment

--- PinnacleWrapper/PinnacleWrapper/Data/Request/UrlSegment.cs
+++ PinnacleWrapper/PinnacleWrapper/Data/Request/UrlSegment.cs
@@ -6,14 +6,24 @@ using System.Threading.Tasks;
 
 namespace PinnacleWrapper.Data.Request
 {
-    public static class RequestStringSegment
+    public static class UrlSegment
     {
-        /*---------- OTHERS -------------*/
-        public static string StraightLie()
+        /*---------- LINE -------------*/
+        public static string StraightLine(StraightLineRequest request)
         {
             var sb = new StringBuilder("v1/line");
+            sb.AppendFormat("?leagueid={0}", request.LeagueId);
+            sb.AppendFormat("&oddsformat={0}", request.OddsFormat);
+            sb.AppendFormat("&sportid={0}", request.SportId);
+            sb.AppendFormat("&eventid={0}", request.EventId);
+            sb.AppendFormat("&periodnumber={0}", request.PeriodNumber);
+            sb.AppendFormat("&bettype={0}", request.BetType);
 
-            return "!";
+            if (request.Handicap != null) sb.AppendFormat("&handicap={0}", request.Handicap);
+            if (request.Team != null) sb.AppendFormat("&team={0}", request.Team);
+            if (request.Side != null) sb.AppendFormat("&side={0}", request.Side);
+
+            return sb.ToString();
         }
 
         /*---------------- OTHERS METHODS ----------------*/
